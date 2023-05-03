12fe64b1dcb3d2960b2eee88cf70b7a6ccfba4f4
vitalfocheux <vitalfocheux@gmail.com>
2023-05-03 10:01:34 +0200

    Ajout des commentaires pour la javadoc

--- src/jeu_console/Position.java
+++ src/jeu_console/Position.java
@@ -4,6 +4,8 @@
 package jeu_console;
 
 /**
+ * Describe a position on a map by means of a row number and a 
+ * column number, both in interval [;]
  * @author Vital FOCHEUX
  *
  */
@@ -11,20 +13,26 @@ public class Position implements Comparable<Position>{
 	
 	private int row, col;
 	
+	/**
+	 * Builds a position with <i>row</i> as the row number and
+	 * <i>col</i> as the column number.
+	 * @param row the row number of the position
+	 * @param col the column number of the position
+	 */
 	public Position(int row, int col) {
 		this.row = row;
 		this.col = col;
 	}
 
 	/**
-	 * @return the row
+	 * @return the row number of the position
 	 */
 	public int getRow() {
 		return row;
 	}
 
 	/**
-	 * @return the col
+	 * @return the column number of the position
 	 */
 	public int getCol() {
 		return col;
@@ -51,15 +59,24 @@ public class Position implements Comparable<Position>{
 	
 	
 
-	/**
-	 * 
-	 * @param that
-	 * @return
+	/** Tests if position <i>this</i> equals position <i>o</i>.
+	 * @return true if <i>o</i> is a position and <i>this</i> and <i>o</i> have same row
+	 * number and column number, false otherwise.
+	 * @param o an object (supposedly a Position) to be compared with the current one
 	 */
-	public boolean equals(Position that) {
-		return (this.getCol() == that.getCol() && this.getRow() == that.getRow());
+	public boolean equals(Object that) {
+		if(! (that instanceof Position)) {
+			return false;
+		}
+		return (this.getCol() == ((Position)that).getCol() && this.getRow() == ((Position)that).getRow());
 	}
 
+	/**
+	 * Builds a String representation of a position as the row number
+	 * enclosed in square brackets, followed be the column number
+	 * enclosed in square brackets. 
+	 * @return the String representation of the position.
+	 */
 	@Override
 	public String toString() {
 		return "["+ row +", "+ col + "]";
