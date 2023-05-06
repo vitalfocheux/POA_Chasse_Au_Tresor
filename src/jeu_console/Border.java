2dccee6e036d2341cb48b37bada034e6bd7558f1
vitalfocheux <vitalfocheux@gmail.com>
2023-05-06 23:55:37 +0200

    Modification du process de Border pour que les ronds sur les coins soient prioritaires par rapport aux autres

--- src/jeu_console/Border.java
+++ src/jeu_console/Border.java
@@ -23,18 +23,26 @@ public class Border extends Fixed{
 	public void process(Character c) {
 		Position pos = c.getNextPosition(c.getDir());
 		if(pos.getCol() == 0) {
-			if(pos.getRow() == 0 || c.getDir() == 6) {
+			if(pos.getRow() == 0) {
 				c.setDir(8);
-			}else if(pos.getRow() == 9 || c.getDir() == 4) {
+			}else if(pos.getRow() == 9) {
+				c.setDir(2);
+			}else if(c.getDir() == 6) {
+				c.setDir(8);
+			}else if(c.getDir() == 4) {
 				c.setDir(2);
 			}else if(c.getDir() == 5){
 				c.setDir(1);
 			}
 		}else if(pos.getCol() == 9) {
-			if(pos.getRow() == 0 || c.getDir() == 8) {
+			if(pos.getRow() == 0) {
 				c.setDir(6);
-			}else if(pos.getRow() == 9 || c.getDir() == 2) {
+			}else if(pos.getRow() == 9) {
+				c.setDir(4);
+			}else if(c.getDir() == 8) {
 				c.setDir(6);
+			}else if(c.getDir() == 2) {
+				c.setDir(4);
 			}else if(c.getDir() == 1){
 				c.setDir(5);
 			}
