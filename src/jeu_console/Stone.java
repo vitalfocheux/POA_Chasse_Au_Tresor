a68d75dead4b843360f451ddbd6edbe0f0758db2
vitalfocheux <vitalfocheux@gmail.com>
2023-05-09 14:10:40 +0200

    Initialisation de Stone

---
+++ src/jeu_console/Stone.java
@@ -0,0 +1,20 @@
+package jeu_console;
+
+public class Stone extends Fixed{
+	
+	public Stone(Position pos) {
+		super(pos);
+	}
+	
+	@Override
+	public String toString() {
+		return"#";
+	}
+
+	@Override
+	public void process(Character c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
