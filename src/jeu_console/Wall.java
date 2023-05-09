dff6075e1bfde1c45d3597acefecb45a1b75a236
vitalfocheux <vitalfocheux@gmail.com>
2023-05-09 14:13:52 +0200

    Initialisation de Wall

---
+++ src/jeu_console/Wall.java
@@ -0,0 +1,16 @@
+package jeu_console;
+
+public class Wall extends Fixed{
+
+	public Wall(Position pos) {
+		super(pos);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void process(Character c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
