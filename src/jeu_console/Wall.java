479c33c3e0b3f1fff9d689753b65074d571228f4
vitalfocheux <vitalfocheux@gmail.com>
2023-05-11 14:23:06 +0200

    calcul de nouvelle direction temporaire

--- src/jeu_console/Wall.java
+++ src/jeu_console/Wall.java
@@ -1,16 +1,53 @@
 package jeu_console;
 
-public class Wall extends Fixed{
+import java.util.*;
 
-	public Wall(Position pos) {
-		super(pos);
-		// TODO Auto-generated constructor stub
+public class Wall{
+	
+	private LinkedList<Stone> stones;
+	private int orientation; //0 : horizontal / 1 : vertical
+
+	public Wall(LinkedList<Stone> stones, int orientation) {
+		this.stones = stones;
+		this.orientation = orientation;
 	}
-
-	@Override
-	public void process(Character c) {
-		// TODO Auto-generated method stub
-		
+	
+	public Wall() {
+		stones = new LinkedList<Stone>();
 	}
-
+	
+	public void addStone(Stone stone) {
+		stones.add(stone);
+		Collections.sort(stones);
+	}
+	
+	/*public boolean isInWall(Stone stone) {
+		for(Stone st : stones) {
+			if(st.getPos().equals(stone.getPos())) {
+				return true;
+			}
+		}
+		return false;
+	}*/
+	
+	public int getOrientation() {
+		return orientation;
+	}
+	
+	public int getDirectionTemp(Stone stone) {
+		if(orientation == 0) {
+			if(Math.abs(stones.getLast().getPos().getCol() - stone.getPos().getCol()) < Math.abs(stones.getFirst().getPos().getCol() - stone.getPos().getCol())) {
+				return 1;
+			}else {
+				return 5;
+			}
+		}else {
+			if(Math.abs(stones.getLast().getPos().getRow() - stone.getPos().getRow()) < Math.abs(stones.getFirst().getPos().getRow() - stone.getPos().getRow())) {
+				return 3;
+			}else {
+				return 7;
+			}
+		}
+	}
+	
 }
