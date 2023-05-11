f2c834945931c2b5dbebb9025f7d76ce158c07f8
vitalfocheux <vitalfocheux@gmail.com>
2023-05-11 14:19:46 +0200

    process de Stone

--- src/jeu_console/Stone.java
+++ src/jeu_console/Stone.java
@@ -1,9 +1,15 @@
 package jeu_console;
 
-public class Stone extends Fixed{
+public class Stone extends Fixed implements Comparable<Stone>{
+	
+	private Wall wall;
 	
 	public Stone(Position pos) {
 		super(pos);
+	}
+	
+	public void addWall(Wall wall) {
+		this.wall = wall;
 	}
 	
 	@Override
@@ -13,8 +19,19 @@ public class Stone extends Fixed{
 
 	@Override
 	public void process(Character c) {
-		// TODO Auto-generated method stub
-		
+		if(c.haveAlreadyLadder()) {
+			c.walk(c.getNextPosition(c.getDir()));
+		}else if(c.haveAlreadyPickaxe()) {
+			c.walk(c.getNextPosition(c.getDir()));
+			c.usePickaxe();
+		}else {
+			c.setDirTemp(wall.getDirectionTemp(this));
+		}
+	}
+
+	@Override
+	public int compareTo(Stone o) {
+		return this.getPos().compareTo(o.getPos());
 	}
 
 }
