7514e493dd1c1a30243bab1b95799e3bd9c350a3
Elian <elian.badstuber@edu.univ-fcomte.fr>
2023-05-18 22:29:36 +0200

    Ajout de la Javadoc + une petite fonction qui factorise le code

--- src/jeu_console/RoadMap.java
+++ src/jeu_console/RoadMap.java
@@ -4,7 +4,7 @@
 package jeu_console;
 
 /**
- * @author Vital FOCHEUX
+ * @author Vital FOCHEUX & Elian BADSTÜBER
  *
  */
 public class RoadMap extends Fixed {
@@ -17,15 +17,30 @@ public class RoadMap extends Fixed {
 		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * @author elian BADSTÜBER
+	 * 
+	 * @param angle the angle calculated from tangent
+	 * @param low the lower limit of the tested interval
+	 * @param upp the upper limit of the tested interval
+	 * @return
+	 */
+	private boolean isBetween(double angle, double low, double upp) {
+		return (angle > low && upp >= angle);
+	}
+	
+	/**
+	 * @author elian BADSTÜBER
+	 * 
+	 * @return the direction to the treasure, using tangent
+	 */
 	public int getDirToTreasure() {
 		
 		int row = treasure.getPos().getRow() - this.getPos().getRow();
 		int col = treasure.getPos().getCol() - this.getPos().getCol();
-		System.out.println("\n\nrow : " + row + "\ncol : " + col + "\n");
 		
 		double angle;
 		double tan = (double) col / (double) row;
-		System.out.println("tan : " + tan + "\n");
 		
 		if (row == 0) {
 			if (col > 0) return 1;
@@ -37,51 +52,20 @@ public class RoadMap extends Fixed {
 		}
 		
 		angle = Math.atan(tan);
-		System.out.println("angle : " + angle + "\n");
 		
 		if (row < 0) {
-			System.out.println("test de l'intervalle : [" + -4*Math.PI/8 + " ; " + -3*Math.PI/8 + "] censé retourner 1\n");
-			if (angle > -4*Math.PI/8 && -3*Math.PI/8 >= angle) {
-				return 1;
-			}
-			System.out.println("test de l'intervalle : [" + -3*Math.PI/8 + " ; " + -Math.PI/8 + "] censé retourner 2\n");
-			if (angle > -3*Math.PI/8  && -Math.PI/8 >= angle) {
-				return 2;
-			}
-			System.out.println("test de l'intervalle : [" + -Math.PI/8 + " ; " + Math.PI/8 + "] censé retourner 3\n");
-			if (angle > -Math.PI/8  && Math.PI/8 >= angle) {
-				return 3;
-			}
-			System.out.println("test de l'intervalle : [" + Math.PI/8 + " ; " + 3*Math.PI/8 + "] censé retourner 4\n");
-			if (angle > Math.PI/8  && 3*Math.PI/8 >= angle) {
-				return 4;
-			}
-			System.out.println("test de l'intervalle : [" + 3*Math.PI/8 + " ; " + 4*Math.PI/8 + "] censé retourner 5\n");
-			if (angle > 3*Math.PI/8  && 4*Math.PI/8 >= angle) {
-				return 5;
-			}
+			if (isBetween(angle, -4*Math.PI/8, -3*Math.PI/8)) return 1;
+			if (isBetween(angle, -3*Math.PI/8, -Math.PI/8)) return 2;
+			if (isBetween(angle, -Math.PI/8, Math.PI/8)) return 3;
+			if (isBetween(angle, Math.PI/8, 3*Math.PI/8)) return 4;
+			if (isBetween(angle, 3*Math.PI/8, 4*Math.PI/8)) return 5;
 		}
 		else {
-			System.out.println("test de l'intervalle : [" + -4*Math.PI/8 + " ; " + -3*Math.PI/8 + "] censé retourner 5\n");
-			if (angle > -4*Math.PI/8 && -3*Math.PI/8 >= angle) {
-				return 5;
-			}
-			System.out.println("test de l'intervalle : [" + -3*Math.PI/8 + " ; " + -Math.PI/8 + "] censé retourner 6\n");
-			if (angle > -3*Math.PI/8  && -Math.PI/8 >= angle) {
-				return 6;
-			}
-			System.out.println("test de l'intervalle : [" + -Math.PI/8 + " ; " + Math.PI/8 + "] censé retourner 7\n");
-			if (angle > -Math.PI/8  && Math.PI/8 >= angle) {
-				return 7;
-			}
-			System.out.println("test de l'intervalle : [" + Math.PI/8 + " ; " + 3*Math.PI/8 + "] censé retourner 8\n");
-			if (angle > Math.PI/8  && 3*Math.PI/8 >= angle) {
-				return 8;
-			}
-			System.out.println("test de l'intervalle : [" + 3*Math.PI/8 + " ; " + 4*Math.PI/8 + "] censé retourner 1\n");
-			if (angle > 3*Math.PI/8  && 4*Math.PI/8 >= angle) {
-				return 1;
-			}
+			if (isBetween(angle, -4*Math.PI/8, -3*Math.PI/8)) return 5;
+			if (isBetween(angle, -3*Math.PI/8, -Math.PI/8)) return 6;
+			if (isBetween(angle, -Math.PI/8, Math.PI/8)) return 7;
+			if (isBetween(angle, Math.PI/8, 3*Math.PI/8)) return 8;
+			if (isBetween(angle, 3*Math.PI/8, 4*Math.PI/8)) return 1;
 		}
 		return -1;
 	}
